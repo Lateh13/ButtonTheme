dfd4d444dd79f8d114ba32d1186f42f3c4927126
Lauri <hakulinen.lauri@gmail.com>
2021-11-10 20:00:39 +0000

    First commit

--- src/HeaderButton.js
+++ src/HeaderButton.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ThemeContext from './ThemeContext';
 
 function HeaderButton() {
+  const themeContext = React.useContext(ThemeContext)
+  const [button, setButton] = useState(themeContext)
+  
+  const buttonColor = () => {
+    if(button === themeContext.black) {
+      setButton(themeContext.blue)
+    } else {
+      setButton(themeContext.black)
+    }
+    return button
+  }
+
   return (
     <div>
-      <button>Press me</button>
+      <button onClick={buttonColor} style={button}>Press me to change Color!</button>
     </div>
   );
 }
